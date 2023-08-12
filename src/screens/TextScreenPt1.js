8db6a9b6d5ba6066de62e46269f964ba8daf56ea
Jonathan <jargumedo.aj@gmail.com>
2023-08-12 13:12:33 -0600

    Finished state and reducer course. Started styling course.

--- src/screens/TextScreenPt1.js
+++ src/screens/TextScreenPt1.js
@@ -12,26 +12,19 @@ import {View, Text, StyleSheet, TextInput} from 'react-native';
  * We can use it to get/use the users input. So inside that callback function, we will also set 
  * the state variable via the state callback function.
  *  */
-const checkPassowrd = (password) => {
-    return password.length < 5 ? true : false;
-};
 const TextScreen = () => {
-    const [password, setPassoword] = useState('');
+    const [name, setName] = useState('');
     return(
         <View>
-            <Text style={{marginLeft: 15, fontSize: 15}}>Enter a password: </Text>
+            <Text style={{marginLeft: 15, fontSize: 15}}>Enter your name: </Text>
             <TextInput 
                 style={styles.textInputBox}
                 autoCapitalize='none'
                 autoCorrect={false}
-                value={password}
-                onChangeText={(newValue) => setPassoword(newValue)}
+                value={name}
+                onChangeText={(newValue) => setName(newValue)}
             />
-            {
-                checkPassowrd(password)
-                ? <Text style={{marginLeft: 15, fontSize: 15}}>The password must contain 5 characters.</Text>
-                : <Text style={{marginLeft: 15, fontSize: 15}}>Password is valid.</Text>
-            }
+            <Text style={{marginLeft: 15, fontSize: 15}}>My name is: {name}</Text>
         </View>
     );
 };
